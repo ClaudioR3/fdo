15a30e2638272fa6fa39741e1f86748f1212ab5e
D3US000 <silente.volley3@gmail.com>
2016-05-23 14:49:35 +0200

    add all class

--- EsamePOOGiugno2010/src/esame/Autore.java
+++ EsamePOOGiugno2010/src/esame/Autore.java
@@ -1,5 +1,17 @@
 package esame;
 
 public class Autore {
-
+	private int annoNascita;
+	private String nome;
+	
+	public Autore(String nome, int annoNascita) {
+		this.nome = nome;
+		this.annoNascita = annoNascita;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public int getAnnoNascita() {
+		return this.annoNascita;
+	}
 }
--- EsamePOOGiugno2010/src/esame/Libro.java
+++ EsamePOOGiugno2010/src/esame/Libro.java
@@ -2,19 +2,24 @@ package esame;
 
 import java.util.*;
 public class Libro {
-private Set<Autore> autori;
-private String titolo;
-public Libro(String titolo) {
-this.titolo = titolo;
-this.autori = new HashSet<Autore>();
-}
-public void addAutore(Autore autore){
-this.autori.add(autore);
-}
-public Set<Autore> getAutori() {
-return Collections.unmodifiableSet(this.autori);
-}
-public String getTitolo(){
-return this.titolo;
-}
+	
+	private Set<Autore> autori;
+	private String titolo;
+	
+	public Libro(String titolo) {
+		this.titolo = titolo;
+		this.autori = new HashSet<Autore>();
+	}
+	
+	public void addAutore(Autore autore){
+		this.autori.add(autore);
+	}
+	
+	public Set<Autore> getAutori() {
+		return Collections.unmodifiableSet(this.autori);
+	}
+	
+	public String getTitolo(){
+		return this.titolo;
+	}
 }
