c19098ed771c35c2358b7bb240996916bb6ce18a
ClaudioR <claudio.ronchetti3@libero.it>
2016-11-15 22:53:47 +0100

    Added comments and Docstrings into DBlink and Document classes

--- Sagitta/DBlink.py
+++ Sagitta/DBlink.py
@@ -7,10 +7,14 @@ import mysql.connector as mysql
 from Document import Document
 class DBlink:
     '''
-    classdocs
+    This class links the others classes with a database. The database's informations are into the document 
+    'config.txt' rappresenting by class Document called config.
     '''
     
     def __init__(self):
+        '''
+        Initialization of object Document for configuration data and all default info
+        '''
         self.config=Document("config.txt")
         self.default_host='www.medcordex.eu'
         self.default_user='sagitta'
@@ -18,39 +22,71 @@ class DBlink:
         self.default_db='medcordex'
         
     def set_config(self,params):
+        '''
+        Set new params of config Document
+        @param params: dictionary
+        '''
         self.config.update(params)
     
     def get_config_toString(self):
+        '''
+        Take the params of config Document
+        @return params: string, params of config Document
+        '''
         return self.config.toString()
     
     def del_config(self):
+        '''
+        Delete all info of the config Document
+        '''
         self.config.delete()
         
     def get_doc(self):
+        '''
+        @return config: Document object, config Document
+        '''
         return self.config
     
     def get_table(self):
+        '''
+        @return table: string, table info into config Document
+        '''
         return self.config.get_parameter("table")
     
     def get_db(self):
+        '''
+        @return db: string, db info into config Document
+        '''
         return self.config.get_parameter("db")
     
     def get_url(self):
+        '''
+        @return url info: string token by 'config' Document. If url is a void string and the host info is equals 
+        to www.medcordex.eu return the default url for medcordex users.
+        '''
         url= self.config.get_parameter("url")
         if url=="":
+            #default url for medcordex users
             if self.config.get_parameter("host")=="www.medcordex.eu":
                 url="ftp://"+self.config.get_parameter("user")+":"+self.config.get_parameter("passwd")+"@"+self.config.get_parameter("host")+"/ALL/"
         return url
         
     def send_query(self,query):
-        # default configuration for medcordex users
+        '''
+        Send query using mysql.connector module. To send the query, the function needs the informations
+        host, user, passwd and db token by 'config' Document. If the host and db info are void string, the function send the query
+        to default info.
+        @param query: string in form like SELECT fields FROM table WHERE conditions
+        @return tuple: query response
+        '''
         if self.config.get_parameter("host")=='' and self.config.get_parameter('db')=='':
+            # default configuration for medcordex users
             database=mysql.connect(host=self.default_host,
                                      user=self.default_user,
                                      passwd=self.default_passwd,
                                      db=self.default_db)
-        # presonal configuration for all other cases
         else:
+            # personal configuration for all other cases
             database=mysql.connect(host=self.config.get_parameter("host"),
                                      user=self.config.get_parameter("user"),
                                      passwd=self.config.get_parameter("passwd"),
@@ -58,4 +94,3 @@ class DBlink:
         cursore=database.cursor()
         cursore.execute(query)
         return cursore.fetchall()
-        
--- Sagitta/Document.py
+++ Sagitta/Document.py
@@ -6,10 +6,17 @@ Created on Jul 12, 2016
 import os
 
 class Document:
+    '''
+    This class reads and writes into a text file. Converts the datas into file in a dictionary like {key:value}. 
+    '''
     def __init__(self,name,path=os.getenv('HOME')):
-	try:
-            #for linux and mac systems
-       	    self.name=name
+        '''
+        The initialization decides the directory to save the file. If the OS is Linux or Mac OS it saves into 
+        pathToHome/.Directory/fileName.txt otherwise into pathToUSERNAME/Documents/Directory/fileName.txt (default path).
+        '''
+        try:
+            #for linux and Mac systems
+            self.name=name
             self.path=path
             self.params=self.read()
         except TypeError:
@@ -18,6 +25,10 @@ class Document:
             self.params=self.read()
 
     def create_file(self):
+        '''
+        Create the file into path.
+        @return the information into file (read)
+        '''
         #create the path if not exists
         if not os.path.exists(self.path):
             os.makedirs(self.path)
@@ -42,6 +53,11 @@ class Document:
         return new_string	
 
     def read(self):
+        '''
+        Read the file path/fileName in form : string1;string2\n then it inserts the info in a dictionary
+        in this form {string1:string2}
+        @return params: dictionary, info into 'fileName' Document
+        '''
         try:
             f= open(os.path.join(self.path,self.name), "rb")
             doc=f.read()
@@ -55,14 +71,23 @@ class Document:
                     params[lista_riga[0]]=lista_riga[1]
             return params
         except :
+            #if the file are not found
             return self.create_file()
     
     def update(self,params):
+        '''
+        Update self.params in new params. Update is not set.
+        @param params: dictionary, the params that need update
+        '''
         for p in params:
             self.params[p]=params[p]
         self.write(self.params)
     
     def write(self,params):
+        '''
+        Write the params {key:value} into file in path/fileName in this form: key;value\n 
+        @param params: dictionary.
+        '''
         doc = open(os.path.join(self.path,self.name), "wb")
         for p in params.keys():
             riga = "%s;%s\n"%(p,params[p])
@@ -72,22 +97,40 @@ class Document:
         self.set_params(self.read())
         
     def delete(self):
+        '''
+        Delete all data into file path/fileName
+        '''
         doc = open(os.path.join(self.path,self.name), "w")
         doc.write("")
         doc.close()
     
     def get_params(self):
+        '''
+        @return params: dictionary.
+        '''
         return self.params
     
     def set_params(self,params):
+        '''
+        Set params with the new params
+        @param params : dictionary
+        '''
         self.params=params
     
     def get_parameter(self,key):
+        '''
+        Return the value of key into params (dictionary).
+        @param key: string
+        @return value: string
+        '''
         if key in self.params:
             return self.params[key]
         return ""
 
     def toString(self):
+        '''
+        @return s: string, params in string type
+        '''
         s=""
         keyset=self.get_params().keys()
         keyset.sort()
@@ -105,6 +148,11 @@ class KL_Document(Document):
         Document.__init__(self, name,path)
         
     def read(self):
+        '''
+        Read the file path/fileName in form : string1;string2;string3;...;stringN then it inserts the info in a dictionary
+        in this form {string1:[string2,...,stringN]}
+        @return params: dictionary {key:[values]}
+        '''
         #self.params= a map like {key:[list]}
         try:
             doc= open(os.path.join(self.path,self.name), "r").read()
